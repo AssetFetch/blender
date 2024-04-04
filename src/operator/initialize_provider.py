2c70f3c89ddc96ab5870fa7a142496458b68b6e0
Lennart Demes <31403260+struffel@users.noreply.github.com>
2024-04-04 14:06:09 +0200

    Better exception handling for initialization

--- src/operator/initialize_provider.py
+++ src/operator/initialize_provider.py
@@ -1,7 +1,11 @@
+import logging
 import bpy
 
 from ..property.templates import AF_VariableQueryUpdateTarget
 from ..util import http
+
+LOGGER = logging.getLogger("af-init")
+LOGGER.setLevel(logging.DEBUG)
 
 class AF_OP_InitializeProvider(bpy.types.Operator):
 	"""Performs the initialization request to the provider and sets the provider settings, if requested."""
@@ -19,6 +23,8 @@ class AF_OP_InitializeProvider(bpy.types.Operator):
 
 	def execute(self,context):
 		af  = bpy.context.window_manager.af
+
+		LOGGER.info(f"Initializing for {af.current_init_url}")
 
 		# Reset existing connection_state
 		if 'current_provider_initialization' in af:
@@ -39,47 +45,51 @@ class AF_OP_InitializeProvider(bpy.types.Operator):
 		if 'current_implementation_list_index' in af:
 			af['current_implementation_list_index'] = 0
 
+		try:
+			# Contact initialization endpoint and get the response
+			query = http.AF_HttpQuery(uri=af.current_init_url,method="get")
+			response : http.AF_HttpResponse = query.execute()
 
-		# Contact initialization endpoint and get the response
-		query = http.AF_HttpQuery(uri=af.current_init_url,method="get")
-		response : http.AF_HttpResponse = query.execute()
-
-		# Set the provider id
-		if "id" in response.parsed:
-			af.current_provider_initialization.name = response.parsed['id']
-		else:
-			raise Exception("No provider ID.")
-
-		# Get the provider text (title and description)
-		if "text" in response.parsed['data']:
-			af.current_provider_initialization.text.configure(response.parsed['data']['text'])
-			
-		# Provider configuration
-		af.current_provider_initialization.provider_configuration.headers.clear()
-		if "provider_configuration" in response.parsed['data']:
-
-			provider_config = response.parsed['data']['provider_configuration']
-
-			# Headers
-			if len(provider_config['headers']) > 0:
-				af.current_connection_state.state = "awaiting_input"
-				for header_info in provider_config['headers']:
-					current_header = af.current_provider_initialization.provider_configuration.headers.add()
-					current_header.configure(header_info)
+			# Set the provider id
+			if "id" in response.parsed:
+				af.current_provider_initialization.name = response.parsed['id']
 			else:
+				raise Exception("No provider ID.")
+
+			# Get the provider text (title and description)
+			if "text" in response.parsed['data']:
+				af.current_provider_initialization.text.configure(response.parsed['data']['text'])
+				
+			# Provider configuration
+			af.current_provider_initialization.provider_configuration.headers.clear()
+			if "provider_configuration" in response.parsed['data']:
+
+				provider_config = response.parsed['data']['provider_configuration']
+
+				# Headers
+				if len(provider_config['headers']) > 0:
+					af.current_connection_state.state = "awaiting_input"
+					for header_info in provider_config['headers']:
+						current_header = af.current_provider_initialization.provider_configuration.headers.add()
+						current_header.configure(header_info)
+				else:
+					af.current_connection_state.state = "connected"
+
+				# Status endpoint
+				af.current_provider_initialization.provider_configuration.connection_status_query.configure(provider_config['connection_status_query'])
+			else:
+				# No configuration required, we can immediately start getting the asset list (assuming the operator polling succeeded)
 				af.current_connection_state.state = "connected"
 
-			# Status endpoint
-			af.current_provider_initialization.provider_configuration.connection_status_query.configure(provider_config['connection_status_query'])
-		else:
-			# No configuration required, we can immediately start getting the asset list (assuming the operator polling succeeded)
-			af.current_connection_state.state = "connected"
-
-		# asset_list_query
-		if "asset_list_query" in response.parsed['data']:
-			af.current_provider_initialization.asset_list_query.configure(response.parsed['data']['asset_list_query'],update_target=AF_VariableQueryUpdateTarget.update_asset_list_parameter)
-			af.current_asset_list.already_queried = False
-		else:
-			raise Exception("No Asset List Query!")
+			# asset_list_query
+			if "asset_list_query" in response.parsed['data']:
+				af.current_provider_initialization.asset_list_query.configure(response.parsed['data']['asset_list_query'],update_target=AF_VariableQueryUpdateTarget.update_asset_list_parameter)
+				af.current_asset_list.already_queried = False
+			else:
+				raise Exception("No Asset List Query!")
+		except Exception as e:
+			af.current_connection_state.state = "connection_error"
+			LOGGER.error(e)
+			raise e
 		
 		return {'FINISHED'}
