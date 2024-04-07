3596b96ccebab26fa22f456d99fdc21d48985b6f
Lennart Demes <31403260+struffel@users.noreply.github.com>
2024-04-07 15:24:45 +0200

    Setup-script for unix

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ pip install --target ./src/lib/ -r ./requirements.txt
 
 # Download the latest json schema version for AssetFetch
 # Change the -b parameter to use a different branch/tag
-New-Item -ItemType Directory -Force -Path ./tmp
+mkdir ./tmp
 git -C ./tmp/ clone -b main --single-branch https://github.com/AssetFetch/spec.git 
-Copy-Item -Recurse -Force -Path ./tmp/spec/json-schema/ -Destination ./src/
-Remove-Item -Force -Recurse -Path ./tmp
+cp -r ./tmp/spec/json-schema/ ./src/
+rm -rf ./tmp
