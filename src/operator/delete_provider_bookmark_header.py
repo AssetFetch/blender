de77f29e64da763a248e93e21591f97e2e19bfe1
Lennart Demes <31403260+struffel@users.noreply.github.com>
2024-05-24 18:24:26 +0200

    Start adding better comments

--- src/operator/delete_provider_bookmark_header.py
+++ src/operator/delete_provider_bookmark_header.py
@@ -2,15 +2,22 @@ import bpy
 from ..property.preferences import *
 
 class AF_OP_DeleteProviderBookmarkHeader(bpy.types.Operator):
-	"""Deletes the currently selected header from the provider bookmark."""
+	"""Deletes the currently selected header from the provider bookmark. Used in addon preferences."""
 
 	bl_idname = "af.delete_provider_bookmark_header"
 	bl_label = "Delete Bookmark"
 	bl_options = {"REGISTER", "INTERNAL"}
 
 	def execute(self, context):
+
+		# Get preferences
 		prefs : AF_PR_Preferences = AF_PR_Preferences.get_prefs()
+
+		# Load the currently selected bookmark
 		current_bookmark = prefs.get_current_bookmark_in_preferences()
+
+		# Remove the currenly selected header entry from it
 		current_bookmark.header_values.remove(prefs.provider_bookmarks_headers_index)
 		prefs.provider_bookmarks_headers_index = max(0,prefs.provider_bookmarks_headers_index-1)
+		
 		return {'FINISHED'}
