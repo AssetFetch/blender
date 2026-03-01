c3c5919817318f3fbf01d0c8c72f6e9f04010f71
Lennart Demes <31403260+struffel@users.noreply.github.com>
2026-03-01 10:14:48 +0100

    New build target

--- build.ps1
+++ build.ps1
@@ -1,3 +1,3 @@
-$BLENDER_VERSION = "4.5"
+$BLENDER_VERSION = "5.0"
 & "$ENV:ProgramFiles\Blender Foundation\Blender $BLENDER_VERSION\blender.exe" --command extension build --source-dir $PSScriptRoot/src --verbose
 & "$ENV:ProgramFiles\Blender Foundation\Blender $BLENDER_VERSION\blender.exe" --command extension server-generate --repo-dir $PSScriptRoot
